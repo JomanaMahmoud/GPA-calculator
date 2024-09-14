cb7d26028906326790f91f80ee12f3b121aa8648
JomanaMahmoud <jomanamahmoud53@gmail.com>
2024-09-14 20:05:14 +0300

    Finished the front end mostly but the back end is still not finished

--- app/src/main/java/com/example/germangpacalculator/MainActivity.java
+++ app/src/main/java/com/example/germangpacalculator/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.germangpacalculator;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +14,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private LinearLayout coursesBox;
+    private Button addCourseButton;
+    private Button calculate;
+    private TextView GPA;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +29,52 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        coursesBox = findViewById(R.id.courses);
+        addCourseButton = findViewById(R.id.addCourse);
+        calculate = findViewById(R.id.calculateGPA);
+        GPA = findViewById(R.id.GPA);
+
+        addCourseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                LinearLayout courseData = new LinearLayout(MainActivity.this);
+                courseData.setOrientation(LinearLayout.HORIZONTAL);
+                LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+                layoutParams.setMargins(10, 0, 10, 0);
+
+
+                EditText courseName = new EditText(MainActivity.this);
+                courseName.setHint("Course Name");
+                courseName.setLayoutParams(layoutParams);
+
+                EditText creditHours = new EditText(MainActivity.this);
+                creditHours.setHint("Credit Hours");
+                creditHours.setLayoutParams(layoutParams);
+
+                EditText gpa = new EditText(MainActivity.this);
+                gpa.setHint("GPA");
+                gpa.setLayoutParams(layoutParams);
+
+                Button deleteCourse = new Button(MainActivity.this);
+                deleteCourse.setText("Delete");
+
+                courseData.addView(courseName);
+                courseData.addView(creditHours);
+                courseData.addView(gpa);
+                courseData.addView(deleteCourse);
+                coursesBox.addView(courseData);
+
+                deleteCourse.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        // Remove the course row from the parent layout
+                        coursesBox.removeView(courseData);
+                    }
+                });
+            }
+        });
     }
+
 }
